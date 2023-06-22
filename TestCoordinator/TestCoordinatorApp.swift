a2c75d68d5de3bb7ae66b8a50df01ccd9e0f01af
andres <alozano@applaudostudios.com>
2023-06-22 15:18:13 -0500

    Initial Commit

---
+++ TestCoordinator/TestCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCoordinatorApp.swift
+//  TestCoordinator
+//
+//  Created by Andres Lozano on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TestCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
