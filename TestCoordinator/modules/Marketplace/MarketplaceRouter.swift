a20bf425524117c6f1b13bdb134bed4180a52862
andres <alozano@applaudostudios.com>
2023-07-26 18:14:42 -0500

    .

--- TestCoordinator/modules/Marketplace/MarketplaceRouter.swift
+++ TestCoordinator/modules/Marketplace/MarketplaceRouter.swift
@@ -1,30 +1,52 @@
 //
 //  MarketplaceRouter.swift
-//  TestCoordinator
 //
-//  Created by Andres Lozano on 22/06/23.
+//  Copyright (c) Andres F. Lozano
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
-import ALCoordinator
+import SUICoordinator
 import SwiftUI
 
-enum MarketplaceRouter: NavigationRouter {
-    
-    case home(viewModel: MarketplaceViewModel)
-    
-    // MARK: NavigationRouter
-    var transition: NavigationTransitionStyle {
-        switch self {
-            case .home:
-                return .push
-        }
+enum MarketplaceRouter: NavigationRoute {
+  
+  case home(viewModel: MarketplaceViewModel)
+  
+  
+  // ---------------------------------------------------------------------
+  // MARK: NavigationRouter
+  // ---------------------------------------------------------------------
+  
+  
+  var transition: NavigationTransitionStyle {
+    switch self {
+      case .home:
+        return .push
     }
-    
-    func view() -> any View {
-        switch self {
-            case .home(let vm):
-                return MarketplaceHomeView(viewModel: vm)
-        }
+  }
+  
+  func view() -> any View {
+    switch self {
+      case .home(let vm):
+        return MarketplaceHomeView(viewModel: vm)
     }
+  }
 }
